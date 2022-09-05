548aee20544595dbede6f15a379ff6e9b5a0792c
Fred Farias <fredmfarias@gmail.com>
2022-09-04 23:23:10 -0300

    COlocando o tomcat embedded para facilitar a execução

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 bolao_trovao
 ============
+
+#REQUISITOS
+
+JDK 8
+https://www.azul.com/downloads/?version=java-8-lts&os=windows&architecture=x86-64-bit&package=jdk
+
+MAVEN 3.5
+https://archive.apache.org/dist/maven/maven-3/3.5.3/binaries/
+
+#Libs que não são mais encontradas no repository maven devem ser manualmente adcionadas ao classpath
+\src\main\resources\lib
--- src/main/java/bolao/services/impl/BolaoService.java
+++ src/main/java/bolao/services/impl/BolaoService.java
@@ -19,8 +19,8 @@ public class BolaoService implements IBolaoService, Serializable{
 	private static final long serialVersionUID = -1870079055351517970L;
 	
 	public static final int DIA = 14;
-	public static final int MES = 6;
-	public static final int ANO = 2018;
+	public static final int MES = 11;
+	public static final int ANO = 2022;
 	public static final int HORA = 10;
 	public static final int MIN = 00;
 	
