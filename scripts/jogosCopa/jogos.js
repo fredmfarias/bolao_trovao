803020571b4433a4226b02373138da97c043a1d4
Fred Farias <fredmfarias@gmail.com>
2022-11-03 22:37:15 -0300

    ajuste coluna nome no ranking

--- scripts/jogosCopa/jogos.js
+++ scripts/jogosCopa/jogos.js
@@ -12,15 +12,12 @@ var findGrupo = function(selecao){
         for(var i of grupo.classificacao){
             
             if( selecao === i.nome_popular){
-                console.log("ACHOOOu " + grupo.nome_grupo.replace('Grupo ', ''));
                 return grupo.nome_grupo.replace('Grupo ', '');
             }
         }
     }
     return;
 }
-
-console.log(findGrupo('Brasil'));
 
 var numJogo = 1;
 rodadas.rodadas.forEach((rodada) => {
@@ -30,7 +27,7 @@ rodadas.rodadas.forEach((rodada) => {
     rodada.jogos.forEach((jogo) => {
         
         fs.appendFileSync(FILE, 
-            `INSERT INTO tb_jogo (id_clube_casa, id_clube_visitante, dataJogo, id_estadio, numeroJogo, rodada, grupo, pesoPontucao)\n VALUES ((select id from tb_clube where nome = '${jogo.equipes.mandante.nome_popular}'), (select id from tb_clube where nome = '${jogo.equipes.visitante.nome_popular}'), to_timestamp('${jogo.data_realizacao.replace('T', ' ')}', 'YYYY-MM-DD HH24:MI'), (select id from tb_estadio where descricao = '${jogo.sede.nome_popular}'), ${numJogo}, ${rodada.rodada}, 'C', ${(jogo.equipes.mandante.nome_popular == 'Brasil' || jogo.equipes.visitante.nome_popular == 'Brasil' ? 2 : 1)});\n`);
+            `INSERT INTO tb_jogo (id_clube_casa, id_clube_visitante, dataJogo, id_estadio, numeroJogo, rodada, grupo, pesoPontucao)\n VALUES ((select id from tb_clube where nome = '${jogo.equipes.mandante.nome_popular}'), (select id from tb_clube where nome = '${jogo.equipes.visitante.nome_popular}'), to_timestamp('${jogo.data_realizacao.replace('T', ' ')}', 'YYYY-MM-DD HH24:MI'), (select id from tb_estadio where descricao = '${jogo.sede.nome_popular}'), ${numJogo}, ${rodada.rodada}, '${findGrupo(jogo.equipes.mandante.nome_popular)}', ${(jogo.equipes.mandante.nome_popular == 'Brasil' || jogo.equipes.visitante.nome_popular == 'Brasil' ? 2 : 1)});\n`);
 			
 			numJogo++;
     });
